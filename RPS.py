e0462bcb0f49662054466c94171d69247d018742
Evelyn <64677331+mievenfall@users.noreply.github.com>
2024-12-08 09:01:17 -0800

    my solution for rps

--- RPS.py
+++ RPS.py
@@ -1,10 +1,30 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
+counter = {'R': 'P', 'P': 'S', 'S': 'R'}
+moves = {}
+
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    if prev_play != "":
+        opponent_history.append(prev_play)
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    hist = opponent_history
 
-    return guess
+    guess = 'R'
+
+    n = 6
+    
+    if len(hist) > n:
+        sequence = "".join(hist[-n:])
+
+        moves["".join(hist[-(n+1):])] = moves.get("".join(hist[-(n+1):]), 0) + 1
+
+        possibilities = [sequence+"R", sequence+"P", sequence+"S"]
+
+        sub_moves = { k: moves[k] for k in possibilities if k in moves.keys()}
+
+
+        if sub_moves:
+            predict = max(sub_moves, key=sub_moves.get)
+            guess = predict[-1]
+
+    return counter[guess]
